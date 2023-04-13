a31eddd661d5c4f5e159dd3ce5e9c9e1672aab92
rnavarrete2 <rnavarrete2@huskers.unl.edu>
2023-04-12 22:42:34 -0500

    Fixed Error

--- Tests/UnitTests.cs
+++ Tests/UnitTests.cs
@@ -36,7 +36,7 @@ namespace GithubActionsLab
         public void Subtract_Valid()
         {
             Assert.AreEqual(-1, Program.Subtract("1", "2"));
-            Assert.AreEqual(0, Program.Subtract("3", "2"));
+            Assert.AreEqual(1, Program.Subtract("3", "2"));
             Assert.AreEqual(2, Program.Subtract("10", "8"));
         }
 
